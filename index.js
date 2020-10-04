b1199bd9c2e0170392e8d5d16654e82bf018df90
SimonaGlom <sglombikova@gmail.com>
2020-10-04 22:39:31 +0200

    Add mapping languages with id

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const fs = require('fs')
+const dataEN = require('./data/dataENfromSKlinks.json')
+const dataDE = require('./data/dataDEfromSKlinks.json')
+
 
 async function parseIntoSeparateFiles(lang, pathToWrite, pathToRead) {
     const readable = fs.createReadStream(pathToRead, { encoding: 'utf8' });
@@ -27,5 +30,49 @@ async function parseIntoSeparateFiles(lang, pathToWrite, pathToRead) {
     file.end();
 }
 
-parseIntoSeparateFiles('en', './data/dataENfromSklinks.json', './data/skwiki-latest-langlinks.sql');
-parseIntoSeparateFiles('de', './data/dataDEfromSklinks.json', './data/skwiki-latest-langlinks.sql');
+//parseIntoSeparateFiles('en', './data/dataENfromSklinks.json', './data/skwiki-latest-langlinks.sql');
+//parseIntoSeparateFiles('de', './data/dataDEfromSklinks.json', './data/skwiki-latest-langlinks.sql');
+
+async function parseLangLinks(lang, pathToWrite, pathToRead) {
+    const readable = fs.createReadStream(pathToRead, { encoding: 'utf8' });
+    const file = fs.createWriteStream(pathToWrite)
+    let  result = {'words': []} 
+    //{{}{}{}}
+
+    const regex = new RegExp(`\\(([0-9]+),(.*?),(.*?),(.*?)\\)`, 'g');
+    let regexIntoLangLinks = new RegExp(`\(id,\'(sk|en|de)\',\'(.*?)\'\)`, 'g');
+
+    for await (const chunk of readable) {
+        let m;
+        while ((m = regex.exec(chunk)) !== null) {
+            let objects = []
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
+
+            objects.push({
+                'id': m[1],
+                'lang': lang,
+                'value': m[3]
+            })
+
+            if (dataEN[+m[1]]) {
+                objects.push(dataEN[+m[1]])
+            }
+
+            if (dataDE[+m[1]]) {
+                objects.push(dataDE[+m[1]])
+            }
+
+            result.words = [...result.words, ...objects]
+
+            break;
+        }
+        break;
+    }
+ 
+    file.write(JSON.stringify(result, null, ' '))
+    file.end(); 
+}
+
+parseLangLinks('sk', './data/resultFromSK.json', './data/skwiki-latest-page.sql')
