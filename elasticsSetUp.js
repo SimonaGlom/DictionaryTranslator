42834a95d6e4059c39b6a036ed078cb291238eee
SimonaGlom <sglombikova@gmail.com>
2020-10-19 08:45:21 +0200

    Add analyzer to elasticsearch

---
+++ elasticsSetUp.js
@@ -0,0 +1,53 @@
+
+var elasticsearch = require('elasticsearch');
+
+var client = new elasticsearch.Client({
+    hosts: ['http://localhost:9200'],
+    apiVersion: "6.8"
+});
+
+client.indices.delete({
+    index: "dictionary"
+})
+
+/**
+ * standard - Unicode Text Segmentation algorithm
+ * filter - lowercase
+ */
+client.indices.create({
+    index: "dictionary",
+    body: {
+        "settings": {
+            "analysis": {
+                "analyzer": {
+                    "my_custom_analyzer": {
+                        "type": "custom",
+                        "tokenizer": "standard",
+                        "filter": [
+                            "lowercase"
+                        ]
+                    }
+                }
+            }
+        },
+        "mappings": {
+            "_doc": {
+                "properties": {
+                    "id": { "type": "text" },
+                    "lang": { "type": "text" },
+                    "value": { "type": "text" },
+                    "translations": {
+                        "type": "nested", "properties": {
+                            "id": { "type": "text" },
+                            "lang": { "type": "text" },
+                            "value": { "type": "text" },
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}, function (err, resp, respcode) {
+    console.log(err, resp, respcode);
+});
