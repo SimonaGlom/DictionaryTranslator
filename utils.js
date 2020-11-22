5f65b7e07abcba9dc6b8b9d133bb518f787f43de
SimonaGlom <sglombikova@gmail.com>
2020-11-22 22:12:08 +0100

    Add removeUnderline function

---
+++ utils.js
@@ -0,0 +1,19 @@
+// Edit titles
+
+// Wikipedia:Dog -> Dog
+function removeCategory(title) {
+    return title.split(':').length === 1 ? title.split(':')[0] : title.split(':')[1]
+}
+
+// Dog (animal) -> Dog
+function removeDescription(title) {
+    return title.split('(')[0].trim()
+}
+
+//Small_dog => Small dog
+function removeUnderline(title) {
+    return (title.split("'")[1]) ? title.split("'")[1].split("_").join(' ') : title
+}
+
+
+module.exports = { removeCategory, removeDescription, removeUnderline}
